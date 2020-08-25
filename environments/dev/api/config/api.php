c21f2321947c52e800816486022d5c71e41b793d
Tao <taoyu65@gmail.com>
2020-08-24 21:34:55 -0700

    update

---
+++ environments/dev/api/config/api.php
@@ -0,0 +1,65 @@
+<?php
+
+$params = array_merge(
+    require __DIR__ . '/../../common/config/params.php',
+    require __DIR__ . '/../../common/config/params-local.php',
+    require __DIR__ . '/params.php',
+    require __DIR__ . '/params-local.php'
+);
+
+$config = [
+    'id' => 'app-api',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'components' => [
+        'request' => [
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ],
+            'cookieValidationKey' => '4JxHr4ADtF-bgLRuCd3IBt1PUdPx0eSa',
+            'csrfParam' => '_apiCSRF',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+//            'enableStrictParsing' => true,
+            'showScriptName' => false,
+            'rules' => [
+//                [
+//                    'class' => 'yii\rest\UrlRule',
+//                    'controller' => ['prime-chat'],
+//                    'except' => ['get'],
+//                    'tokens' => [
+//                        '{id}' => '<id:\\w+>'
+//                    ],
+//                    'extraPatterns' => [
+//                        'POST prime-chat' => 'prime-chat/send',
+//                    ]
+//                ],
+                '<controller:[\w|\-]+>/<action:[\w|\-]+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:[\w|\-]+>/<action:[\w|\-]+>' => '<controller>/<action>',
+            ],
+        ],
+        'user' => [
+            'class' => 'common\components\User',
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => false,
+        ],
+    ],
+    'modules' => [
+        'v1' => [
+            'class' => 'api\modules\v1\Module',
+        ],
+    ],
+    'params' => $params,
+];
+
+return $config;
