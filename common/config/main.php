fe4de6c64177e18716a728bfd590e00929108702
Tao <taoyu65@gmail.com>
2020-08-24 17:47:31 -0700

    update

--- common/config/main.php
+++ common/config/main.php
@@ -6,6 +6,15 @@ return [
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'urlManagerFrontend' => [
+            'baseUrl' => 'http://' . (YII_ENV_PROD ? '' : ''),
+            'class' => 'yii\web\UrlManager',
+            'enableStrictParsing' => false,
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => array_merge(require(dirname(dirname(__DIR__)).'/frontend/config/url_rules.php')
+            ),
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
---
+++ frontend/config/url_rules.php
@@ -0,0 +1,7 @@
+<?php
+return [
+
+    //'<controller:\w+>/<id:\d+>' => '<controller>/view',
+    //'<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+    '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+];
